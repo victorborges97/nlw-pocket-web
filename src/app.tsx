f4dd19b42c9428e426861ff6c77fa03a3e414114
Joao Victor Borges <borges.jvdo@gmail.com>
2024-09-10 15:17:38 -0300

    comment import

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,6 @@
 import { Dialog } from './components/ui/dialog'
 import { CreateGoal } from './components/create-goal'
-import { EmptyGoals } from './components/empty-goals'
+//import { EmptyGoals } from './components/empty-goals'
 import { Summary } from './components/summary'
 
 export function App() {
