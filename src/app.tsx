b65b47600adc6ca23c41b2248e73d73d2ff53e69
Joao Victor Borges <borges.jvdo@gmail.com>
2024-09-12 15:49:47 -0300

    feat: get summary home

--- src/app.tsx
+++ src/app.tsx
@@ -1,13 +1,23 @@
 import { Dialog } from './components/ui/dialog'
 import { CreateGoal } from './components/create-goal'
-//import { EmptyGoals } from './components/empty-goals'
 import { Summary } from './components/summary'
+import { EmptyGoals } from './components/empty-goals';
+import { useQuery } from '@tanstack/react-query';
+import { getSummary } from './http/get-summary';
 
 export function App() {
+  const { isLoading, data } = useQuery({
+    queryKey: ["summary"],
+    queryFn: getSummary,
+    staleTime: 1000 * 60,//60 seconds
+  })
   return (
     <Dialog>
-      {/* <EmptyGoals /> */}
-      <Summary />
+      {
+        isLoading 
+        ? "Loading..."
+        : data && data?.total > 0 ? <Summary /> : <EmptyGoals />
+      }
       <CreateGoal />
     </Dialog>
   )
--- src/components/summary.tsx
+++ src/components/summary.tsx
@@ -6,14 +6,38 @@ import { Progress, ProgressIndicator } from './ui/progress-bar'
 import { Separator } from './ui/separator'
 import { OutlineButton } from './ui/outline-button'
 import { DayWeeek } from './day-week'
+import { getSummary } from '../http/get-summary'
+import { useQuery } from '@tanstack/react-query'
+import dayjs from 'dayjs'
+import ptBR from 'dayjs/locale/pt-BR'
+import { PendingGoals } from './pedding-goals'
+
+dayjs.locale(ptBR);
 
 export function Summary() {
+  const { data } = useQuery({
+    queryKey: ["summary"],
+    queryFn: getSummary,
+    staleTime: 1000 * 60,//60 seconds
+  })
+
+  if(!data) {
+    return null;
+  }
+
+  const firstDayOfWeek = dayjs().startOf("week").format("D MMM");
+  const endDayOfWeek = dayjs().endOf("week").format("D MMM");
+
+  const completedPercentage = Math.round(data.completed * 100 / data.total);
+
   return (
     <div className="py-10 max-w-[480px] px-5 mx-auto flex flex-col gap-6">
       <div className="flex items-center justify-between">
         <div className="flex items-center gap-3">
           <InOrbitIcon />
-          <span className="text-lg font-semibold">5 a 10 de Agosto</span>
+          <span className="text-lg font-semibold capitalize">{firstDayOfWeek}</span>
+          -
+          <span className="text-lg font-semibold capitalize">{endDayOfWeek}</span>
         </div>
         <DialogTrigger asChild>
           <Button size="sm">
@@ -24,49 +48,40 @@ export function Summary() {
       </div>
 
       <div className="flex flex-col gap-3">
-        <Progress value={8} max={15}>
+        <Progress value={data?.completed ?? 0} max={data?.total ?? 0}>
           <ProgressIndicator
             style={{
-              width: 200,
+              width: `${completedPercentage}%`,
             }}
           />
         </Progress>
 
         <div className="flex items-center justify-between text-sm text-zinc-400">
           <span>
-            Você completou <span className="text-zinc-100">8</span> de{' '}
-            <span className="text-zinc-100">15</span> metas nessa semana.
+            Você completou <span className="text-zinc-100">{data?.completed ?? 0}</span> de{' '}
+            <span className="text-zinc-100">{data?.total ?? 0}</span> metas nessa semana.
           </span>
-          <span>50%</span>
+          <span>{completedPercentage}%</span>
         </div>
       </div>
 
       <Separator />
 
-      <div className="flex flex-wrap gap-3">
-        <OutlineButton>
-          <Plus className="size-4 text-zinc-600" />
-          Meditar
-        </OutlineButton>
-
-        <OutlineButton>
-          <Plus className="size-4 text-zinc-600" />
-          Nadar
-        </OutlineButton>
-
-        <OutlineButton>
-          <Plus className="size-4 text-zinc-600" />
-          Praticar exercícios
-        </OutlineButton>
-      </div>
+      <PendingGoals />
 
       <div className="flex flex-col gap-6">
         <h2 className="text-xl font-medium">Sua semana</h2>
 
-        <DayWeeek dayWeek="Domingo" />
-        <DayWeeek dayWeek="Segunda" />
-        <DayWeeek dayWeek="Terça" />
+        {
+          data && Object.entries(data.goalsPerDay)
+          .map(([ date, goals ]) => <DayWeeek 
+            key={date} 
+            date={date}
+            goals={goals}
+          />)
+        }
       </div>
     </div>
   )
 }
+
