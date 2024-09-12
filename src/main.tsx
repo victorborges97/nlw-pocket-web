ac5223f4f163652abc8d7b20abadc5a487e50cf1
Joao Victor Borges <borges.jvdo@gmail.com>
2024-09-12 15:47:58 -0300

    feat: add config react-query

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,14 @@ import { createRoot } from 'react-dom/client'
 import { App } from './app'
 import './index.css'
 
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <QueryClientProvider client={queryClient}>
     <App />
+    </QueryClientProvider>
   </StrictMode>
 )
